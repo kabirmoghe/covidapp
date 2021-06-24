9dd6ecd7fb25c847f535bcf8478c46a5c9fa0bfb
kabirmoghe <64380076+kabirmoghe@users.noreply.github.com>
2021-06-24 07:46:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,70 +1,79 @@
 from flask import Flask, redirect, url_for, render_template, request
 import covidapp
-import dataset
 import os.path
 from os import path
-import vaxx
+import readbucketdata
 import pandas as pd
 
 
 app = Flask(__name__)
-app.secret_key = 'hello'
+app.secret_key = 'SECRET KEY'
 
 @app.route("/")
 def home():
-	if path.exists("vaxxdataset.csv") == False:
-		vaxx.create_vaxx_data().to_csv('vaxxdataset.csv')
+
+	readbucketdata.readbucketdata('vaxx').to_csv('vaxxdataset.csv')
+	readbucketdata.readbucketdata('full').to_csv('fulldataset.csv')
+
 	return render_template("index.html")
+	
 
-
-@app.route("/countyinfo", methods = ['POST', 'GET'])
+@app.route("/data", methods = ['POST', 'GET'])
 def countyinfo():
-	if path.exists("vaxxdataset.csv") == False:
-		vaxx.create_vaxx_data().to_csv('vaxxdataset.csv')
+
+	if path.exists('vaxxdataset.csv') == False and path.exists('fulldataset.csv') == False:
+		readbucketdata.readbucketdata('vaxx').to_csv('vaxxdataset.csv')
+		readbucketdata.readbucketdata('full').to_csv('fulldataset.csv')
 	
 	if request.method == "POST":
 		county = request.form["cty"]
 		if county == '':
 			return render_template('empty.html')
-
-		if path.exists("fulldataset.csv") == False:
-			dataset.main_function().to_csv('fulldataset.csv')
-
-		embed_vaxx = covidapp.vaxx_plot(county)
-		allinfo = covidapp.county_stats(county)
-		if len(allinfo) == 9:
-			tbl, stat, info, rec, risk_pos, pct, y_n_mask, mask_details, color = allinfo
-			ctyrisk_pos = risk_pos - 90
-
-			if county.split(', ')[1] == 'TX' or county.split(', ')[1] == 'HI':
-				note = 'Neither Hawaii nor Texas provide county-level data on vaccinations, hence why the visualization below is empty.'
-
-			else:
-				note = 'The visualization below shows the percentage of fully vaccinated people within the county broken down by age group.'
-
-			return render_template("result.html", county = county, tbl = [tbl.to_html(classes='data', header = True)], stat = stat, info = info, rec = rec, risk_pos = risk_pos, pct = pct, ctyrisk_pos = ctyrisk_pos, y_n_mask = y_n_mask, mask_details = mask_details, color = color, note = note)
 		else:
-			return render_template("undef_result.html", issue = allinfo)
+
+			data1, data2 = pd.read_csv('fulldataset.csv'), pd.read_csv('vaxxdataset.csv')
+
+			if county in data1['County Name'].values and county in data2['County Name'].values:
+
+				covidapp.vaxx_plot(county)
+				covidapp.avg_plot(county)
+				allinfo = covidapp.county_stats(county)
+				
+				tbl, stat, info, rec, risk_pos, pct, y_n_mask, mask_details, color, risk = allinfo
+
+				ctyrisk_pos = risk_pos - 12.5
+
+				if round(pct) == 100.0:
+					ptile = 'Top ~99%'
+				else:
+					if pct >= 50:
+						ptile = 'Top ~{}%'.format(str(round(100.0-pct)))
+					else:
+						ptile = 'Bottom ~{}%'.format(str(round(pct)))
+
+				if county.split(', ')[1] == 'TX' or county.split(', ')[1] == 'HI':
+					note = 'Neither Hawaii nor Texas provide county-level data on vaccinations, hence why the visualization below is empty.'
+
+				else:
+					note = 'The visualizations below show the percentage of fully vaccinated people within the county broken down by age group.'
+
+				return render_template("result.html", county = county, tbl = [tbl.to_html(classes='data', header = True)], stat = stat, info = info, rec = rec, risk_pos = risk_pos, pct = pct, ctyrisk_pos = ctyrisk_pos, y_n_mask = y_n_mask, mask_details = mask_details, color = color, note = note, ptile = ptile, risk = risk)
+			
+			else:
+				return render_template("undef_result.html", issue = 'Please enter a valid county name (i.e. Orange County, CA). The county you entered, {}, may not have complete information.'.format(county))
 	else:
 		ctys = covidapp.county_list()
 		return render_template("data.html", ctys = ctys)
 		#return render_template("data.html", states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'])
-		
-
-@app.route("/about")
-def about():
-	if path.exists("vaxxdataset.csv") == False:
-		vaxx.create_vaxx_data().to_csv('vaxxdataset.csv')
-
-	return render_template("about.html")
+			
 
 @app.route("/stats", methods = ['POST', 'GET'])
 def stats():
-	if path.exists("vaxxdataset.csv") == False:
-		vaxx.create_vaxx_data().to_csv('vaxxdataset.csv')
+	if path.exists('vaxxdataset.csv') == False and path.exists('fulldataset.csv') == False:
+		readbucketdata.readbucketdata('vaxx').to_csv('vaxxdataset.csv')
+		readbucketdata.readbucketdata('full').to_csv('fulldataset.csv')
+		
 	if request.method == "POST":
-		if path.exists("fulldataset.csv") == False:
-			dataset.main_function().to_csv('fulldataset.csv')
 		choice = request.form["choice"]
 		if choice == 'vaxx':
 			date = covidapp.multivaxx_plot()
@@ -83,8 +92,10 @@ def stats():
 
 @app.route("/explore", methods = ['POST', 'GET'])
 def explore():
-	if path.exists("fulldataset.csv") == False:
-		dataset.main_function().to_csv('fulldataset.csv')
+	if path.exists('vaxxdataset.csv') == False and path.exists('fulldataset.csv') == False:
+		readbucketdata.readbucketdata('vaxx').to_csv('vaxxdataset.csv')
+		readbucketdata.readbucketdata('full').to_csv('fulldataset.csv')
+
 	if request.method == "POST":
 		# FIX LOAD DATASET
 		attr1 = request.form["choice1"]
@@ -94,12 +105,15 @@ def explore():
 		covidapp.scatter(attr1, attr2, trendline)
 
 		return render_template("explore_results.html", attr1 = attr1, attr2 = attr2, trendline = trendline)
-		
+			
 	else:
 		cols = [col for col in pd.read_csv('fulldataset.csv').columns[3:] if col != 'State' and "Mask" not in col]
 		cols.reverse()
 		return render_template("explorehome.html", cols = cols)
 
+@app.route("/about")
+def about():
+	return render_template("about.html")
 
 if __name__ == '__main__':
     app.run(debug = True)
